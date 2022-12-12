24c548ad79218adada814e75f7ea436938c9c7ab
Raghu M N <nraghu@zeomega.com>
2022-12-12 21:50:06 +0530

    Mounting the puppet code into container

--- puppet_container/setup_master.sh
+++ puppet_container/setup_master.sh
@@ -2,4 +2,4 @@
 
 sudo docker pull puppet/puppetserver:6.0.3
 
-sudo docker run --net puppet --name puppet -p8140:8140 -d --hostname puppet-linoxide -v /home/nraghu/work/puppet_master_container/puppet_container/puppet.conf:/etc/puppetlabs/puppet/puppet.conf -v /home/nraghu/work/puppet_master_container/puppet_container/hiera.yaml:/etc/puppetlabs/puppet/hiera.yaml -v /home/nraghu/work/puppet_master_container/puppet_container/fileserver.conf:/etc/puppetlabs/puppet/fileserver.conf -v /home/nraghu/work/code:/etc/puppetlabs/code/ puppet/puppetserver:6.0.3
+sudo docker run --net puppet --name puppet -p8140:8140 -d --hostname puppet-linoxide -v /home/nraghu/work/puppet_master_container/puppet_container/puppet.conf:/etc/puppetlabs/puppet/puppet.conf -v /home/nraghu/work/puppet_master_container/puppet_container/hiera.yaml:/etc/puppetlabs/puppet/hiera.yaml -v /home/nraghu/work/puppet_master_container/puppet_container/fileserver.conf:/etc/puppetlabs/puppet/fileserver.conf -v /home/nraghu/work/code:/ect/puppetlabs/code/environments/ puppet/puppetserver:6.0.3
