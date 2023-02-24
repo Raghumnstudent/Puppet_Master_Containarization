dcb430524bfb13a96cf023bf10a29bc5cd165d36
Raghu M N <nraghu@zeomega.com>
2023-02-24 23:35:03 +0530

    Updating shell script

--- nginx/setup_nginx.sh
+++ nginx/setup_nginx.sh
@@ -5,18 +5,19 @@ sudo docker network create puppet
 sudo docker pull nginx:1.15.5
 
 echo "creating the nginx certificate"
+cwd=`pwd`
 
-openssl genrsa -out /home/nraghu/work/puppet_master_container/nginx/certs/server.key 2048
+openssl genrsa -out $cwd/certs/server.key 2048
 
-openssl req -new -key /home/nraghu/work/puppet_master_container/nginx/certs/server.key -out /home/nraghu/work/puppet_master_container/nginx/certs/server.csr -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
+openssl req -new -key $cwd/certs/server.key -out $cwd/certs/server.csr -subj '/CN=www.mydom.com/O=My Company Name LTD./C=US'
 
-openssl x509 -req -in /home/nraghu/work/puppet_master_container/nginx/certs/server.csr -signkey /home/nraghu/work/puppet_master_container/nginx/certs/server.key -out /home/nraghu/work/puppet_master_container/nginx/certs/server.crt
+openssl x509 -req -in $cwd/certs/server.csr -signkey $cwd/certs/server.key -out $cwd/certs/server.crt
 
-#sudo sh -c "echo -n 'raghu:' >> /home/nraghu/nginx/.htpasswd"
-#sudo sh -c "openssl passwd -apr1 >> /home/nraghu/nginx/.htpasswd"
+sudo sh -c "echo -n 'raghu:' >> $cwd/.htpasswd"
+sudo sh -c "openssl passwd -apr1 >> $cwd/.htpasswd"
 
 echo "creating the nginx container"
 
-sudo docker run --name test_nginx -d --net puppet -v /home/nraghu/work/puppet_master_container/nginx/default.conf:/etc/nginx/conf.d/default.conf -v /home/nraghu/work/puppet_master_container/nginx/.htpasswd:/etc/nginx/.htpasswd -v /home/nraghu/work/puppet_master_container/nginx/nginx.conf:/etc/nginx/nginx.conf -v /home/nraghu/work/puppet_master_container/nginx/certs/server.csr:/etc/nginx/server.csr -v /home/nraghu/work/puppet_master_container/nginx/certs/server.key:/etc/nginx/server.key -v /home/nraghu/work/puppet_master_container/nginx/certs/server.crt:/etc/nginx/server.crt -v /home/nraghu/packages/:/home/ -p 8141:8141 nginx:1.15.5
+sudo docker run --name test_nginx -d --net puppet -v $cwd/default.conf:/etc/nginx/conf.d/default.conf -v $cwd/.htpasswd:/etc/nginx/.htpasswd -v $cwd/nginx.conf:/etc/nginx/nginx.conf -v $cwd/certs/server.csr:/etc/nginx/server.csr -v $cwd/certs/server.key:/etc/nginx/server.key -v $cwd/certs/server.crt:/etc/nginx/server.crt -v $cwd/:/home/ -p 8141:8141 nginx:1.15.5
 
 
--- puppet_container/setup_master.sh
+++ puppet_container/setup_master.sh
@@ -1,5 +1,7 @@
-#sudo docker network create puppet
+sudo docker network create puppet
 
 sudo docker pull puppet/puppetserver:6.0.3
+cwd=`pwd`
+echo $cwd
 
-sudo docker run --net puppet --name puppet -p8140:8140 -d --hostname puppet-linoxide -v /home/nraghu/work/puppet_master_container/puppet_container/puppet.conf:/etc/puppetlabs/puppet/puppet.conf -v /home/nraghu/work/puppet_master_container/puppet_container/hiera.yaml:/etc/puppetlabs/puppet/hiera.yaml -v /home/nraghu/work/puppet_master_container/puppet_container/fileserver.conf:/etc/puppetlabs/puppet/fileserver.conf -v /home/nraghu/work/code:/ect/puppetlabs/code/environments/ puppet/puppetserver:6.0.3
+sudo docker run --net puppet --name puppet -p8140:8140 -d --hostname puppet-linoxide -v $cwd/puppet.conf:/etc/puppetlabs/puppet/puppet.conf -v $cwd/hiera.yaml:/etc/puppetlabs/puppet/hiera.yaml -v $cwd/fileserver.conf:/etc/puppetlabs/puppet/fileserver.conf -v $cwd/code:/ect/puppetlabs/code/environments/ puppet/puppetserver:6.0.3
